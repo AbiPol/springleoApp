9201ee5be6bd1bbe4ec47f2f4e8243906057bff4
AbiPol <35527096+AbiPol@users.noreply.github.com>
2018-01-24 23:43:11 +0000

    Roles y users

--- src/main/java/com/curso/springleo/entity/User.java
+++ src/main/java/com/curso/springleo/entity/User.java
@@ -3,15 +3,80 @@ package com.curso.springleo.entity;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="users")
+@Table(name = "users")
 public class User {
 
+	@Id
+	@Column(name="username" , unique = true , nullable = false , length = 45)
 	private String username;
+	
+	@Column(name="password" , nullable = false , length = 60)
 	private String password;
+	
+	@Column(name = "disponible" , nullable = false)
 	private boolean disponible;
+	
+	@OneToMany(fetch=FetchType.LAZY , mappedBy = "user")
+	// Es un conjunto de elmentos que no se pueden repetir entre ellos.
+	// HashSet es la clase que vamos a utilizar para implementar la interfaz SET
 	private Set<UserRole> userRole = new HashSet<UserRole>();
+
+	public User() {
+	}
+
+	public User(String username, String password, boolean disponible) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.disponible = disponible;
+	}
+
+	public User(String username, String password, boolean disponible, Set<UserRole> userRole) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.disponible = disponible;
+		this.userRole = userRole;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isDisponible() {
+		return disponible;
+	}
+
+	public void setDisponible(boolean disponible) {
+		this.disponible = disponible;
+	}
+
+	public Set<UserRole> getUserRole() {
+		return userRole;
+	}
+
+	public void setUserRole(Set<UserRole> userRole) {
+		this.userRole = userRole;
+	}
+
 }
--- src/main/java/com/curso/springleo/entity/UserRole.java
+++ src/main/java/com/curso/springleo/entity/UserRole.java
@@ -1,12 +1,70 @@
 package com.curso.springleo.entity;
+
+import javax.persistence.Column;
 /*
  * Se encarga de almacenar los roles del usuario
  */
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
 
 @Entity
-@Table(name="user_role")
+@Table(name = "user_role", uniqueConstraints = @UniqueConstraint(columnNames = { "role", "username" }))
 public class UserRole {
+
+	@Id
+	@GeneratedValue
+	@Column(name="user_role_id", unique=true, nullable= false)
+	private Integer userRoleId;
+	
+	//FetchType.LAZY, significa que al acceder a un usuario no se van a cargar todos sus roles, 
+	// solo se cargaran cuando se accedan a ellos en la lectura. Asi no cosumimos recursos del sistema
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinTable(name = "users" ,
+	           joinColumns = @JoinColumn(name="username" , nullable= false))
+	private User user;
+	
+	@Column(name="role" , nullable= false , length = 45)
+	private String role;
+
+	public UserRole() {
+	}
+
+	public UserRole(Integer userRoleId, User user, String role) {
+		super();
+		this.user = user;
+		this.role = role;
+	}
+
+	public Integer getUserRoleId() {
+		return userRoleId;
+	}
+
+	public void setUserRoleId(Integer userRoleId) {
+		this.userRoleId = userRoleId;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
 
 }
